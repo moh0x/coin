ad7dbdb733f4a5ec1da93c922a13f386e36fbc4b
moh0x <141264101+moh0x@users.noreply.github.com>
2025-02-19 05:49:04 -0800

    Update tradeRoute.js

--- routes/trade/tradeRoute.js
+++ routes/trade/tradeRoute.js
@@ -4,9 +4,9 @@ const tradeController = require('../../controller/trade/tradeController');
 const { verifyToken } = require("../../utility/verifyToken");
 const {body,validationResult } = require("express-validator");
 const { verifyUser } = require('../../utility/verify_role_user');
-router.post('/create',body("count").isNumeric().isLength({min:6,max:50}).withMessage("type valid count coin"),body("price").isNumeric().isLength({min:8,max:30}).withMessage("type valid price"),body("priceCoin").isLength({min:6,max:30}).withMessage("type valid price coin"),verifyToken,verifyUser,tradeController.createTradeSell);
-
-router.patch('/buyCoin',body("tradeId").isMongoId().withMessage("type valid trade id"),verifyToken,tradeController.buyCoin);
+router.post('/create',body("count").isNumeric().isLength({min:1,max:50}).withMessage("type valid count coin"),body("priceCoin").isNumeric().isLength({min:1,max:50}).withMessage("type valid price"),verifyToken,verifyUser,tradeController.createTradeSell);
+router.delete('/delete',body("tradeId").isMongoId().withMessage("type valid trade id"),verifyToken,verifyToken,tradeController.deleteMyTradeSellOpen);
+router.patch('/buyCoin',body("tradeId").isMongoId().withMessage("type valid trade id"),verifyToken,verifyToken,tradeController.buyCoin);
 router.get('/getMyTradesSell',verifyToken,verifyUser,tradeController.getMyTradesSell);
 router.get('/getAllTradesSell',verifyToken,verifyUser,tradeController.getAllTradesSell);
 router.get('/getMyTradesBuy',verifyToken,verifyUser,tradeController.getMyTradesBuy);
