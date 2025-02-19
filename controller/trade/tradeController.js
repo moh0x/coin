3587ab51ac2029b3a14de5702d77eb435e634ac9
moh0x <141264101+moh0x@users.noreply.github.com>
2025-02-19 05:49:33 -0800

    Update tradeController.js

--- controller/trade/tradeController.js
+++ controller/trade/tradeController.js
@@ -7,21 +7,25 @@ const bcrypt = require("bcryptjs");
 const {body,validationResult } = require("express-validator");
 const httpsStatus = require('../../constant/httpStatus');
 const { Trade } = require("../../model/trade/Trade");
+const { Agent } = require("../../model/agent/Agent");
 const createTradeSell = async(req,res)=>{
     try {
         const token = req.headers.token;
         const valid = validationResult (req);
-        const count = req.body.count;
-        const price = req.body.price;
-        const priceOneCoin = req.body.priceOneCoin;
+        const count = req.body.count; 
+        const priceOneCoin = req.body.priceCoin;
+        const price = count * priceOneCoin;
         const senderUser = await User.findOne({token:token});
         const coin = await Coin.findOne({name:"Moh"})
   if (valid.isEmpty()) {
-    if (count > 0 && count <=10000) {
+    console.log(priceOneCoin);
+    
+    if (count > 0 && count <= 10000 && priceOneCoin > 0) {
         if (senderUser.coin >= count) {
             if (senderUser.dollars >= 0.25) {
                 const newDollars = senderUser.dollars - 0.25;
-                const newEarn = coin + 0.25;
+                const newCoin = senderUser.coin - count;
+                const newEarn = coin.earn + 0.25;
                 await Coin.findByIdAndUpdate(coin.__v,{
                     $set:{
                         earn:newEarn
@@ -30,7 +34,8 @@ const createTradeSell = async(req,res)=>{
                 await coin.save();
                 await User.findByIdAndUpdate(senderUser._id,{
                     $set:{
-                        dollars:newDollars
+                        dollars:newDollars,
+                        coin:newCoin
                     }
                 });
                 await senderUser.save();
@@ -39,10 +44,12 @@ const createTradeSell = async(req,res)=>{
                     senderId:senderUser._id,
                     count:count,
                     price:price,
-                    priceOneCoin:priceOneCoin
+                    priceOneCoin:priceOneCoin,
+                    reciverId:null
         
                 });
                 await newTrade.save();
+                res.status(200).json({"status":httpsStatus.SUCCESS,data:newTrade});
              } else {
                 res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"0.25$ to open trade"});
              }
@@ -56,10 +63,43 @@ const createTradeSell = async(req,res)=>{
     res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":valid['errors'][0].msg});
   }
     } catch (error) {
+        console.log(error);
+        
         res.status(400).json({"status":httpsStatus.ERROR,"message":"error"});
     }
 }
-
+const deleteMyTradeSellOpen = async(req,res)=>{
+   try {
+    const tradeId= req.body.tradeId;
+    const trade = await Trade.findById(tradeId);
+    const token = req.headers.token;
+    const user = await User.findOne({token:token});
+    
+    if (valid.isEmpty()) {
+        if (trade) {
+            if (trade.senderId == user._id) {
+                if (trade.state == "new") {
+                   await Trade.findByIdAndDelete(tradeId) ;
+                   res.status(200).json({"status":httpsStatus.SUCCESS,data:null});
+                } else {
+                    res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"trade is finished"});
+                }
+            } else {
+                res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"you don't have permission"});
+            }
+        } else {
+            res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"trade not found or already used"});
+        }
+    } else {
+        res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":valid['errors'][0].msg}); 
+         
+    }
+   } catch (error) {
+    res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"}); 
+   }
+   
+    
+}
 const buyCoin = async(req,res)=>{
     try {
         const token = req.headers.token;
@@ -69,21 +109,31 @@ const buyCoin = async(req,res)=>{
         const trade = await Trade.findById(tradeId);
         const coin = await Coin.findOne({name:"Moh"})
     if (valid) {
-        if (trade) {
+        if (trade && trade.state == "new") {
             if (trade.senderId != reciverUser._id) {
                 if (reciverUser.dollars >=trade.price +  0.25) {
                     const newDollarsRecvier = reciverUser.dollars - 0.25;
                     const newCoinsReciver = reciverUser.coin + trade.count;
                     const newEarn = coin.earn + 0.25;
-                    const agentFees = 0;
-                    const coinTradeLastDay = coin.coinTradeLastDay + trade.count;
+                    let agentFees = 0;
+                    const coinTradeLastDay = Number(coin.coinTradeLastDay) + Number(trade.count);
                     const priceAdd = ((trade.price - coin.price)*trade.count)/3000000;
                     const newPrice = coin.price + priceAdd;
                     newPrice.toFixed(10);
                     if (reciverUser.codeAgent != null) {
                         agentFees = trade.price * 0.1;
-                        agentFees.toFixed(2);
+                      
+
+                     const agent =    await Agent.findOneAndUpdate({code:reciverUser.codeAgent});
+                     const newDollards =  agentFees + agent.dollars;
+                     await Agent.findByIdAndUpdate(agent._id,{
+                        $set:{
+                            dollars:newDollards
+                        }
+                     })
+                        await agent.save()
                     }
+                   
                     await Coin.findByIdAndUpdate(coin._id,{
                         $set:{
                             earn:newEarn,
@@ -108,25 +158,28 @@ const buyCoin = async(req,res)=>{
                         }
                     });
                     await senderUser.save();
-                   await Trade.findByIdAndUpdate(tradeId,{
+               const newTrade =    await Trade.findByIdAndUpdate(tradeId,{
                         userNameReciver:reciverUser.userName,
                         reciverId:reciverUser._id,
                         state:"finish"
                     });
                     await newTrade.save();
+                    res.status(200).json({"status":httpsStatus.SUCCESS,data:newTrade});
                  } else {
                     res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"you don't have money"});
                  }
             } else {
-                res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"you con't buy from your self"});
+                res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"you can't buy from your self"});
             }
          } else {
-            res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"trade not found"});
+            res.status(400).json({"status":httpsStatus.FAIL,data:null,"message":"trade not found or already used"});
          }
     } else {
         res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":valid['errors'][0].msg}); 
     }
     } catch (error) {
+        console.log(error);
+        
         res.status(400).json({"status":httpsStatus.ERROR,"message":"error"});
     }
 } 
@@ -146,7 +199,7 @@ const getMyTradesSell = async(req,res)=>{
   }
   const getMyTradesBuy = async(req,res)=>{
     const limit = 15;
-    const page = req.body.page || 1;
+    const page = req.headers.page || 1;
     const skip = (page - 1) * limit;
      try {
       var token =  req.headers.token;
@@ -159,7 +212,7 @@ const getMyTradesSell = async(req,res)=>{
   }
   const getMyTradesSellOpen = async(req,res)=>{
     const limit = 15;
-    const page = req.body.page || 1;
+    const page = req.headers.page || 1;
     const skip = (page - 1) * limit;
      try {
       var token =  req.headers.token;
@@ -173,15 +226,17 @@ const getMyTradesSell = async(req,res)=>{
 
   const getAllTradesSell = async(req,res)=>{
     const limit = 15;
-    const page = req.body.page || 1;
+    const page = req.headers.page || 1;
     const skip = (page - 1) * limit;
      try {
       var token =  req.headers.token;
       const user = await User.findOne({token:token});
-        const trades = await Trade.find({$and:[{senderId:{$ne: user._id}},{reciverId:"      "},{state:"new"}]}).sort({time:-1}).limit(limit).skip(skip); 
+        const trades = await Trade.find({$and:[{senderId:{$ne: user._id}},{state:"new"}]}).sort({time:-1}).limit(limit).skip(skip); 
         res.status(200).json({"status":httpsStatus.SUCCESS,"data":trades}); 
      } catch (error){
+        console.log(error);
+        
       res.status(400).json({"status":httpsStatus.ERROR,"data":null,"message":"error"});
      }
   }
-  module.exports ={createTradeSell,buyCoin,getMyTradesSell,getMyTradesBuy,getMyTradesSellOpen,getAllTradesSell}
+  module.exports ={createTradeSell,buyCoin,getMyTradesSell,getMyTradesBuy,getMyTradesSellOpen,getAllTradesSell,deleteMyTradeSellOpen}
