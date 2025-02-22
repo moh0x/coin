11669a334cb722158e1b32436b78e5590c3ae4b2
moh0x <141264101+moh0x@users.noreply.github.com>
2025-02-22 03:01:16 -0800

    Update coinController.js

--- controller/coin/coinController.js
+++ controller/coin/coinController.js
@@ -9,57 +9,68 @@ const httpsStatus = require('../../constant/httpStatus');
 const { Agent } = require("../../model/agent/Agent");
 const addUserCode =async(req,res)=>{
     try {
-        const token = req.headers.token;
         const valid = validationResult(req);
+    
+        
         if (valid.isEmpty()) {
+            const token = req.headers.token; 
+            const user = await User.findOne({token:token});
+            const coin = await Coin.findOne({name:"Moh"});
             const codeUser = req.body.codeUser;
             const userCode = await User.findOne({code:codeUser});
+          
+         if (userCode) {
             const userCodeDolladrs = userCode.dollars;
-            const user = await User.findOne({token:token});
-            const cois = await Coin.findOne({name:"Moh"});
-         if (user.codeUser == null) {
-            
-            const userNewDollars= user.dollars + 1;
-            await User.findByIdAndUpdate(user._id,{
-                $set:{
-                    codeUser:codeUser,
-                    dollars:userNewDollars
+            if (user.codeUser == null) {
+                if (user.code.toLowerCase() != codeUser.toLowerCase()) {
+                    if (userCode.affilate < 3) {
+                        const userNewDollars= user.dollars + 1;
+                        await User.findByIdAndUpdate(user._id,{
+                            $set:{
+                                codeUser:codeUser,
+                                dollars:userNewDollars
+                            }
+                        })
+                        let newAffilate = userCode.affilate + 1;
+                        if (newAffilate == 3) {
+                           let userCodeDollars = userCodeDolladrs + 1;
+                           await User.findByIdAndUpdate(userCode._id,{
+                               $set:{
+                                   affilate:newAffilate,
+                                   dollars:userCodeDollars
+                               }
+                               
+                           })
+                           await user.save();
+                           await userCode.save();
+                           res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
+                        }else{
+                           await User.findByIdAndUpdate(userCode._id,{
+                               $set:{
+                                   affilate:newAffilate,
+                               }
+                               
+                           })
+                        }
+                        res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
+                       } else {
+                           res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"max is 3"});
+                       } 
+                } else {
+                    res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"don't use your code"});
                 }
-            })
-            if (userCode.affilate < 3) {
-                let newAffilate = userCode.affilate + 1;
-                if (newAffilate == 3) {
-                   let userCodeDollars = userCodeDolladrs + 1;
-                   await User.findByIdAndUpdate(userCode._id,{
-                       $set:{
-                           affilate:newAffilate,
-                           dollars:userCodeDollars
-                       }
-                       
-                   })
-                   await user.save();
-                   await userCode.save();
-                   res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
-                }else{
-                   await User.findByIdAndUpdate(userCode._id,{
-                       $set:{
-                           affilate:newAffilate,
-                       }
-                       
-                   })
-                }
-                res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
-               } else {
-                   res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"max is 3"});
-               }
+             } else {
+                res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"just 1 time"});
+             }
+            } else {
+                res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"there is no user with this code"});
+            }
          } else {
-            res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"just 1 time"});
-         }
-        } else {
             res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":valid['errors'][0].msg});
-        }
+         }
     } catch (error) {
-        res.status(400).json({"status":httpsStatus.ERROR,"message":error});
+      
+        res.status(400).json({"status":httpsStatus.ERROR,"message":"error"});
     }
 }
 const addAgentCode =async(req,res)=>{
@@ -70,34 +81,40 @@ const addAgentCode =async(req,res)=>{
             const codeAgent = req.body.codeAgent;
             const agent = await Agent.findOne({code:codeAgent});
             const user = await User.findOne({token:token});
-            const cois = await Coin.findOne({name:"Moh"});
-         if (user.codeAgent == null) {      
-            const userNewDollars= user.dollars + 1;
-            await User.findByIdAndUpdate(user._id,{
-                $set:{
-                    codeAgent:codeAgent,
-                    dollars:userNewDollars
-                }
-            })
-                let newAffilate = agent.affilate + 1;
-                   await Agent.findByIdAndUpdate(agent._id,{
-                       $set:{
-                           affilate:newAffilate,
-                       }
-                       
-                   })
-                   await user.save();
-                   await agent.save();
-                   res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
-                res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
-         } else {
-            res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"just 1 time"});
-         }
+            const coin = await Coin.findOne({name:"Moh"});
+        if (agent) {
+            if (user.codeAgent == null) {      
+                const userNewDollars= user.dollars + 1;
+                await User.findByIdAndUpdate(user._id,{
+                    $set:{
+                        codeAgent:codeAgent,
+                        dollars:userNewDollars
+                    }
+                })
+                    let newAffilate = agent.affilate + 1;
+                       await Agent.findByIdAndUpdate(agent._id,{
+                           $set:{
+                               affilate:newAffilate,
+                           }
+                           
+                       })
+                       await user.save();
+                       await agent.save();
+                       res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
+                    res.status(200).json({"status":httpsStatus.SUCCESS,"data":null});
+             } else {
+                res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"just 1 time"});
+             }
+        } else {
+            res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":"there is no agent with this code"});
+        }
         } else {
             res.status(400).json({"status":httpsStatus.FAIL,"data":null,"message":valid['errors'][0].msg});
         }
     } catch (error) {
-        res.status(400).json({"status":httpsStatus.ERROR,"message":error});
+        console.log(error);
+        
+        res.status(400).json({"status":httpsStatus.ERROR,"message":"error"});
     }
 }
 module.exports ={addUserCode,addAgentCode}
