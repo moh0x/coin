4ccc6c716546472475cbab24bb1a46836dcf1e34
moh0x <141264101+moh0x@users.noreply.github.com>
2025-02-23 09:17:37 -0800

    Update CoinRoutes.js

--- routes/coin/CoinRoutes.js
+++ routes/coin/CoinRoutes.js
@@ -6,7 +6,7 @@ const {body,validationResult } = require("express-validator");
 const { verifyUser } = require('../../utility/verify_role_user');
 router.patch('/addUserCode',body("codeUser").isLength({min:1,max:6}).withMessage("type valid code"),verifyToken,verifyUser,coinController.addUserCode);
 router.patch('/addAgentCode',body("codeAgent").isLength({min:1,max:6}).withMessage("type valid code"),verifyToken,verifyUser,coinController.addAgentCode);
-
+router.get('/coinInfo',verifyToken,verifyUser,coinController.getCoin);
 
   module.exports = 
     router
