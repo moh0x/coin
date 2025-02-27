a3530894f17e0df106c1b3c7745855a265ec1e3d
moh0x <141264101+moh0x@users.noreply.github.com>
2025-02-27 18:31:38 +0000

    Update depAndWithRouter.js

--- routes/deposit and withdraw/depAndWithRouter.js
+++ routes/deposit and withdraw/depAndWithRouter.js
@@ -4,11 +4,12 @@ const depAndWithController = require('../../controller/deposit and withdraw/depo
 const { verifyToken } = require("../../utility/verifyToken");
 const {body,validationResult } = require("express-validator");
 const { verifyUser } = require('../../utility/verify_role_user');
-router.post('/addDeposit',body("txId").isNumeric().isLength({min:1,max:88}).withMessage("type valid txId"),verifyToken,verifyUser,depAndWithController.deposit);
-router.post('/addWithDraw',body("price").isNumeric().isLength({min:1,max:5}).withMessage("type valid price"),verifyToken,verifyUser,depAndWithController.withdraw);
+router.post('/addDeposit',body("txId").isLength({min:20,max:88}).withMessage("type valid txId"),verifyToken,verifyUser,depAndWithController.deposit);
+router.post('/addWithDraw',body("price").isNumeric().isLength({min:1,max:5}).withMessage("type valid price"),body("wallet").isLength({min:10,max:40}).withMessage("type valid wallet"),verifyToken,verifyUser,depAndWithController.withdraw);
 router.get('/getMyWithDraws',verifyToken,verifyUser,depAndWithController.getWithdraws);
 router.get('/getMyDeposits',verifyToken,verifyUser,depAndWithController.getDeposits);
 
   module.exports = 
     router
+  
   
